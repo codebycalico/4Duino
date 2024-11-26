dc12d3d627b74b1b73393b2537a5c10fa3b468ba
Calico Rose Randall <139941083+codebycalico@users.noreply.github.com>
2024-11-26 10:05:11 -0800

    Screen working!
    Changed the physical pins from the TX and RX "built in" the Arduino Uno to the TX and RX pins defined with the SoftwareSerial in the code.
    Also changed "setBackground" with "replaceBackground" in the loop.
    Screen is working, sliding between red and green every second.

--- src/main.cpp
+++ src/main.cpp
@@ -1,78 +1,60 @@
 // Calico adaptation of examples in 4Duino library
 
 // Required for dtostrf (float-to-string conversion)
-// #include <stdlib.h>
-
-// #include "SoftwareSerial.h"
-// #include "FourDuino.h"
-
-// // Companion header to Color.h, provides a bunch of standard color literals
-// // Stuff like COLOR_WHITE, COLOR_PURPLE, COLOR_SLATEGRAY, COLOR_PAPAYAWHIP, etc
-// // Check out the file to see what is available.
-// #include "Colors.h"
-
+#include <stdlib.h>
+#include "SoftwareSerial.h"
+#include "FourDuino.h"
 #include <FastLED.h>
 #include <Arduino.h>
+#include "OLEDUtil.h"
+
+// Companion header to Color.h, provides a bunch of standard color literals
+// Stuff like COLOR_WHITE, COLOR_PURPLE, COLOR_SLATEGRAY, COLOR_PAPAYAWHIP, etc
+// Check out the file to see what is available.
+#include "Colors.h"
 
 // Avoid repeated calls to oled.getDeviceHeight()
-// uint8_t HEIGHT;
-// // Current write position of the scope graph thing
-// // Scope loops from left to right
-// uint8_t x = 0;
-// // Store previous analog value for drawing graph lines
-// uint16_t lastVal = 0;
+ uint8_t HEIGHT;
+// Current write position of the scope graph thing
+// Scope loops from left to right
+ uint8_t x = 0;
+// Store previous analog value for drawing graph lines
+uint16_t lastVal = 0;
 
-// #define RESET_PIN 8
-// #define TX_PIN 9
-// #define RX_PIN 10
+#define RESET_PIN 8
+#define TX_PIN 9
+#define RX_PIN 10
 
-// // Should work on any device
-// // You can use Serial* but you still have to #include "SoftwareSerial.h"
-// // OLED(RESET_PIN, serialInterface)
-// OLED oled = OLED(RESET_PIN, SoftwareSerial(RX_PIN, TX_PIN));
+// Should work on any device
+// You can use Serial* but you still have to #include "SoftwareSerial.h"
+// OLED(RESET_PIN, serialInterface)
+OLED oled = OLED(RESET_PIN, SoftwareSerial(RX_PIN, TX_PIN));
 
 void setup() {
     pinMode(13, OUTPUT);
 
     Serial.begin(9600);
 
-
-    // oled.init();
-    // oled.setFontOpacity(true); // Blank the area behind text
-    // HEIGHT = oled.getDeviceHeight();
-    // oled.setBackground(Color(0, 200, 0));
-    //Serial.println("Setup complete.");
+    oled.init();
+    oled.setFontOpacity(true); // Blank the area behind text
+    HEIGHT = oled.getDeviceHeight();
 }
 
 void loop(){
-    EVERY_N_MILLISECONDS( 500 ){
+// Set contrast based on photoresistor value
+// Normal room light level produces about 4V
+// Alternatively: oled.setContrast(OLEDUtil::scaleAnalog(analogRead(A3),15));
+    oled.setContrastFromAnalog(A3);
+
+    EVERY_N_MILLISECONDS( 1000 ){
         if(digitalRead(13) == HIGH){
+             oled.replaceBackground(Color(0, 200, 0));
             digitalWrite(13, LOW);
-            Serial.println("Digital write low.");
         } else {
+             oled.replaceBackground(Color(200, 0, 0));
             digitalWrite(13, HIGH);
-            Serial.println("Digital write high.");
+            Serial.println("Pin HIGH");
         }
     }
-    //oled.replaceBackground(Color(255, 0, 0));
-    // // Set contrast based on photoresistor value
-    // // Normal room light level produces about 4V
-    // // Alternatively: oled.setContrast(OLEDUtil::scaleAnalog(analogRead(A3),15));
-    // oled.setContrastFromAnalog(A3);
-
-    // // Clear columns for new oscilloscope data
-    // // drawLine(x1, y1, x2, y2, color)
-    // // draw functions can accept colors as 16-bit color values or Color objects
-    // oled.drawLine(x, 0, x, HEIGHT-1, Color(0,30,0));
-    // oled.drawLine(x+1, 0, x+1, HEIGHT-1, Color(20,60,20)); // lighter color for the 'write head'
-    
-    // // +5V -> Photoresistor -> A3 -> 10kOhm -> GND
-    // uint16_t newVal = analogRead(A3);
-    // // Draw a line from the previous reading to the current one
-    // oled.drawLine(
-    //     x, HEIGHT - 1 - OLEDUtil::scaleAnalog(lastVal, HEIGHT),
-    //     x+1, HEIGHT - 1 - OLEDUtil::scaleAnalog(newVal, HEIGHT),
-    //     COLOR_LIME
-    // );
 
 }
