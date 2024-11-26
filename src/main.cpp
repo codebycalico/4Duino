e79e1b305aa932456428939aecb073d9b28ddf26
Calico Rose Randall <139941083+codebycalico@users.noreply.github.com>
2024-11-25 16:40:19 -0800

    Figured out the Serial issue
    Fixed the Serial issue that wouldn't print. Can't use while (!Serial) in setup with Arduino Uno because the hardware is setup to emulate USB but doesn't actually have the same process. Different than a Teensy, so Serial is automatically connected and not an object that can be called on in the instance of (!Serial).

--- src/main.cpp
+++ src/main.cpp
@@ -1,54 +1,56 @@
 // Calico adaptation of examples in 4Duino library
 
 // Required for dtostrf (float-to-string conversion)
-#include <stdlib.h>
+// #include <stdlib.h>
 
-#include "SoftwareSerial.h"
-#include "FourDuino.h"
+// #include "SoftwareSerial.h"
+// #include "FourDuino.h"
 
-// Companion header to Color.h, provides a bunch of standard color literals
-// Stuff like COLOR_WHITE, COLOR_PURPLE, COLOR_SLATEGRAY, COLOR_PAPAYAWHIP, etc
-// Check out the file to see what is available.
-#include "Colors.h"
+// // Companion header to Color.h, provides a bunch of standard color literals
+// // Stuff like COLOR_WHITE, COLOR_PURPLE, COLOR_SLATEGRAY, COLOR_PAPAYAWHIP, etc
+// // Check out the file to see what is available.
+// #include "Colors.h"
 
 #include <FastLED.h>
+#include <Arduino.h>
 
 // Avoid repeated calls to oled.getDeviceHeight()
-uint8_t HEIGHT;
-// Current write position of the scope graph thing
-// Scope loops from left to right
-uint8_t x = 0;
-// Store previous analog value for drawing graph lines
-uint16_t lastVal = 0;
+// uint8_t HEIGHT;
+// // Current write position of the scope graph thing
+// // Scope loops from left to right
+// uint8_t x = 0;
+// // Store previous analog value for drawing graph lines
+// uint16_t lastVal = 0;
 
-#define RESET_PIN 8
-#define TX_PIN 9
-#define RX_PIN 10
+// #define RESET_PIN 8
+// #define TX_PIN 9
+// #define RX_PIN 10
 
-// Should work on any device
-// You can use Serial* but you still have to #include "SoftwareSerial.h"
-// OLED(RESET_PIN, serialInterface)
-OLED oled = OLED(RESET_PIN, SoftwareSerial(RX_PIN, TX_PIN));
+// // Should work on any device
+// // You can use Serial* but you still have to #include "SoftwareSerial.h"
+// // OLED(RESET_PIN, serialInterface)
+// OLED oled = OLED(RESET_PIN, SoftwareSerial(RX_PIN, TX_PIN));
 
 void setup() {
     pinMode(13, OUTPUT);
-    while (!Serial){
+   
         Serial.begin(9600);
-    }
+
 
     // oled.init();
     // oled.setFontOpacity(true); // Blank the area behind text
     // HEIGHT = oled.getDeviceHeight();
     // oled.setBackground(Color(0, 200, 0));
-    Serial.println("Setup complete.");
+    //Serial.println("Setup complete.");
 }
 
 void loop(){
-    EVERY_N_MILLISECONDS ( 500 ){
-        Serial.println("Every 500 ms.");
-        digitalWrite(13, HIGH);
-    }
+    digitalWrite(13, HIGH);
+    Serial.println("Digital write high.");
+    delay(1000);
     digitalWrite(13, LOW);
+    Serial.println("Digital write low.");
+    delay(1000);
     //oled.replaceBackground(Color(255, 0, 0));
     // // Set contrast based on photoresistor value
     // // Normal room light level produces about 4V
